353cc18e2ca9742034e91fb0c3e8bd4ad93e5cf2
suraj95699 <surajrawat95699>
2022-07-05 12:09:49 +0530

    created static header and sidebar

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/',function(){
+    return view("Welcome");
+});
+Route::get('/view',function(){
+    return view("admin.app");
 });
