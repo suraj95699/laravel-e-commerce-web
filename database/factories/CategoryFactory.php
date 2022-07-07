b00247526b2cd23d0e61e1d986e04db8d97c0060
suraj95699 <surajrawat95699>
2022-07-07 13:14:50 +0530

    categories sec. 1. created model, migrations and insert data using factories.

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Category;
+
+class CategoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    protected $model = Category::class;
+    public function definition()
+    {
+        return [
+            'name'          =>  $this->faker->name,
+            'description'   =>  $this->faker->realText(100),
+            'parent_id'     =>  1,
+            'menu'          =>  1,
+        ];
+    }
+}
