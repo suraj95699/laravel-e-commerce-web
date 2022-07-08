135150053dca41e13f62c017e6f5757acdd13516
suraj95699 <surajrawat95699>
2022-07-08 10:02:06 +0530

    under attribute section1. created attribute model, migration and seeding

---
+++ app/Models/Attribute.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Attribute extends Model
+{
+    use HasFactory;
+     /**
+     * @var string
+     */
+    protected $table = 'attributes';
+
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'code', 'name', 'frontend_type', 'is_filterable', 'is_required'
+    ];
+
+    /**
+     * @var array
+     */
+    protected $casts  = [
+        'is_filterable' =>  'boolean',
+        'is_required'   =>  'boolean',
+    ];
+}
